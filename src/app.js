f8810ea447ecefc3660ff8de8bc6fc5e9e07804c
Achyut <onestring22@gmail.com>
2024-10-29 22:24:49 +0530

    fix error successfully

--- src/app.js
+++ src/app.js
@@ -8,11 +8,10 @@ const app = express();
 app.use(helmet());
 
 app.use(
-  // cors({
-  //   origin: process.env.CORS_ORIGIN,
-  //   credentials: true,
-  // })
-  cors()
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
 );
 app.use(express.json({ limit: "16kb" }));
 
