39edf3fa989a2bd742447eaad7bbf1cfdafe2b53
Achyut <onestring22@gmail.com>
2024-10-30 23:46:38 +0530

    added backup PORT to index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import { configDotenv } from "dotenv";
 import { app } from "./app.js";
 import dbConnect from "./DB/db.js";
 configDotenv();
-console.log(process.env.PORT);
+// console.log(process.env.PORT);
 dbConnect()
   .then(() => {
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT || 3000, () => {
       console.log(`App is listening at port ${process.env.PORT}`);
     });
   })
